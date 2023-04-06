ac31376e934f7c5305e0cf39fe620f592742e408
Valentin <5619@holbertonstudents.com>
2023-04-06 07:08:59 -0700

    0. script that, using this REST API

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+import requests
+from sys import argv
+
+id = argv[1]
+id = int(id)
+
+user = requests.get(f"https://jsonplaceholder.typicode.com/users/{id}").json()
+todo = requests.get("https://jsonplaceholder.typicode.com/todos/").json()
+
+task = 0
+task_valid = 0
+
+for userid in todo:
+    if userid['userId'] == id:
+        task += 1
+        if userid['completed']:
+            task_valid += 1
+
+print(f"Employee {user['name']} is done with tasks({task_valid}/{task})")
+
+for title in todo:
+    if title['completed'] and title['userId'] == id:
+        print(title['title'])
